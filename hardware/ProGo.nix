4a7b1d3e9be468a267f13172af534e1183f55309
localhost <locahost@no.com>
2021-08-07 17:05:17 +0200

    Add hardware configuration for ProGo

---
+++ hardware/ProGo.nix
@@ -0,0 +1,49 @@
+{ config, lib, pkgs, ... }:
+with lib;
+let
+  cfg = config.teletypeOne.hardware.ProGo;
+in
+{
+  options.teletypeOne.hardware.ProGo = mkEnableOption "Enable ProGoYoYo";
+
+  config = mkIf cfg{
+      boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "zroot/root";
+      fsType = "zfs";
+    };
+
+  fileSystems."/nix" =
+    { device = "zroot/root/nix";
+      fsType = "zfs";
+    };
+
+  fileSystems."/home" =
+    { device = "zroot/root/home";
+      fsType = "zfs";
+    };
+
+  fileSystems."/zroot" =
+    { device = "zroot";
+      fsType = "zfs";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/065ef8a9-521f-4f2f-822b-ef024ed05361";
+      fsType = "ext2";
+    };
+
+  fileSystems."/FjordMain" =
+    { device = "zroot/FjordMainCompress";
+      fsType = "zfs";
+    };
+
+  swapDevices = [ ];
+
+
+  };
+}
---
+++ hardware/default.nix
@@ -0,0 +1,24 @@
+{ config, lib, pkgs, ... }:
+with lib;
+let
+  cfg = config.teletypeOne.hardware;
+in
+{
+  options = {};
+  imports =
+    [
+      ./ProGo.nix
+    ];
+  config = {
+    assertions = [
+      {
+        assertion =
+          let
+            selection = mapAttrsToList (system: enabled: {inherit system enabled; }) cfg;
+          in
+            count (x: x.enabled) selection == 1;
+        message = "You must select exactly one hardware configuration";
+      }
+    ];
+  };
+}
