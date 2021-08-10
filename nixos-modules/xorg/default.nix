7912cb2bcc2232ceae1ba8b43707752cd594b4e8
localhost <locahost@no.com>
2021-08-11 01:29:02 +0200

    Add xorg hardware configuration

---
+++ nixos-modules/xorg/default.nix
@@ -0,0 +1,82 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+let
+  xserver-enable = config.services.xserver.enable;
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec -a "$0" "$@"
+  '';
+  cfg = config.teletypeOne.xorg;
+in {
+  options.teletypeOne.xorg = {
+    enable = mkEnbleOption "Xorg so graphical can happen";
+    gpu = mkOption {
+      description = "Which gpu?";
+      type = types.enum ["modesetting" "nvidia"];
+    };
+    nvidia = mkOption {
+      description = "Nvidia yaaay";
+      type = types.submodule {
+        options = {
+          prime = mkEnableOption "Nvidia PRIME";
+
+          intelBusId = mkOption {
+            type = types.str;
+            default = "";
+          };
+          nvidiaBusId = mkOption {
+            type = types.str;
+            default = "";
+          };
+
+        };
+      };
+      default = {};
+    };
+  xmonad = mkEnableOption "Enable xmonad";
+  lightdm = mkEnableOption "Enable lightdm";
+  libinput = mkEnableOption "Enable libinput";
+  };
+  config = mkIf cfg.enable (mkMege [
+    {
+      services.xserver = {
+        enable = true;
+
+        windowManager = mkIf cfg.xmonad {
+          xmonad.enable = true;
+          xmonad.enableContribAndExtras = true;
+        };
+
+        displayManager = mkIf cfg.xmonad {
+          lightdm.enable = mkIf cfg.lightdm true;
+          defaultSession = "none+xmonad";
+        };
+
+        libinput.enable = false;
+
+         libinput' = mkIf cfg.libinput{
+          libinput.enable = true;
+        };
+      };
+      hardware = {
+        opengl.driSupport32Bit = true;
+      };
+    }
+    (mkIf (cfg.gpu == "nvidia") {
+      services.xserver.videoDrivers = ["nvidia"];
+      environment.systemPackages = mkIf (cfg.nvidia.prime)
+        [ nvidia-offload pkgs.libglvnd ];
+
+      hardware.nvidia.prime = mkIf cfg.nvidia.prime {
+        offload.enable = true;
+
+        intelBusId = cfg.nvidia.intelBusId;
+        nvidiaBusId = cfg.nvidia.nvidiaBusId;
+      };
+    })
+  ]);
+}
