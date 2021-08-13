c6f3bc3ed3d9a71bc0ec60c76b26e3e32e86bbf9
localhost <locahost@no.com>
2021-08-14 01:03:56 +0200

    Add ProGo to systems

---
+++ systems/ProGo.nix
@@ -0,0 +1,44 @@
+inputs: {
+  system = "x86_64-linux";
+
+  modules = [
+    ../nixos-modules/default.nix
+    ({ pkgs, config, lib, ... }:
+      let
+        inherit (config.teletypeOne.pkgs) nixpkgs-unstable;
+      in
+        {
+          teletypeOne = {
+            xorg = {
+              enable = true;
+              gpu = "nvidia";
+              xmonad = "true";
+              lightdm = "true";
+              libinput = "true";
+            };
+
+            networking = {
+              bluetooth.enable = true;
+              networkManager = true;
+            };
+
+
+            hardare.ProGo = true;
+            flakes.enable = true;
+
+          };
+        }
+    )
+  ] ++ [
+    ({ pkgs, ... }: {
+      networking = {
+        hostName = "ProGo";
+        useDHCP = false;
+        hostId = "3457b394";
+      };
+
+      time.timeZone = "Europe/Berlin";
+      system.stateVersion = "21.05";
+    })
+  ];
+}
