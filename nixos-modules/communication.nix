d8fde68d602bed3ba1d885823e108f98c93abbda
localhost <locahost@no.com>
2021-08-14 07:45:50 +0200

    Add communication module

---
+++ nixos-modules/communication.nix
@@ -0,0 +1,25 @@
+{ config, lib, pkgs, ... }:
+with lib;
+let
+  cfg = config.teletypeOne.communication;
+in {
+  options.teletypeOne.communication = {
+    free = mkEnableOption "Install free communication tools.";
+    nonFree = mkEnableOption "Install nonfree communication tools.";
+    bullshit = mkEnableOption "Install dumb communication tools.";
+  };
+
+  config = (mkMerge [
+    (mkIf (cfg.free) {
+      environment.systemPackages = with pkgs; [signal-desktop element-desktop];
+    })
+
+    (mkIf (cfg.nonfree) {
+      environment.systemPackages = with pkgs; [discord];
+    })
+
+    (mkIf (cfg.bullshit) {
+      environment.systemPackages = with pkgs; [teams];
+    })
+  ]);
+}
--- nixos-modules/default.nix
+++ nixos-modules/default.nix
@@ -4,6 +4,7 @@
   imports = [
     ./main.nix
     ./util.nix
+    ./communication.nix
     ./fonts.nix
     ./terminals.nix
     ./xorg
