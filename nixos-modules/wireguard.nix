c7fc27202da2f53a2bf53494cde539f074f5edca
localhost <locahost@no.com>
2021-08-28 15:22:00 +0200

    Update Wireguard

--- nixos-modules/wireguard.nix
+++ nixos-modules/wireguard.nix
@@ -10,6 +10,26 @@ in {
   config = (mkMerge [
     (mkIf cfg.enable {
       environment.systemPackages = with pkgs; [ wireguard ];
+      networking.firewall = {
+        allowedUDPPorts = [ 12346 ];
+      };
+      netwoking.wireguard.interfaces = {
+        wg0 = {
+          ips = [ "192.168.77.6" ];
+          listenPort = 12346;
+          adress = "192.168.77.6";
+          privateKeyFile = /home/localhost/wireguard-keys/private;
+
+          peers = [
+            {
+              publicKey = "cRk0cF+PHOvBaBmVe9jy9v+8ClVMGNLXmiHqEYFU/kg=";
+              allowedIPs = ["192.168.77.5"];
+              endpoint = "140.82.33.200:12346";
+              persistentKeepalive = 25;
+            }
+          ];
+        };
+      };
     })
   ]);
 }
