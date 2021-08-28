44134fdf1af87c439fe6582bf5d0dd251928d714
localhost <locahost@no.com>
2021-08-28 17:12:24 +0200

    Maybe

--- nixos-modules/theGrandUtilities.nix
+++ nixos-modules/theGrandUtilities.nix
@@ -3,8 +3,8 @@ with lib;
 let
   cfg = config.teletypeOne.theGrandUtilities;
 in {
-  options.teletyeOne.theGrandUtilities = {
-    enable = mkEnableOption "Install TheGradUtilities (all for \"elderly people\")";
+  options.teletypeOne.theGrandUtilities = {
+    enable = mkEnableOption "Install TheGrandUtilities (all for \"elderly people\")";
   };
 
   config = (mkIf cfg.enable {
