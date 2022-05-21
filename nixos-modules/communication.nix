d855b18c4f54a273cb7819e02a283907e32b7e53
localhost <localhost@irunx.org>
2022-05-21 16:10:57 +0200

    Update discord

--- nixos-modules/communication.nix
+++ nixos-modules/communication.nix
@@ -3,10 +3,10 @@ with lib;
 let
   cfg = config.teletypeOne.communication;
   discord-latest = pkgs.discord.overrideAttrs (old: {
-    version = "0.0.16";
+    version = "0.0.17";
     src = pkgs.fetchurl {
-      url = "https://dl.discordapp.net/apps/linux/0.0.16/discord-0.0.16.tar.gz";
-      sha256 = "sha256-UTVKjs/i7C/m8141bXBsakQRFd/c//EmqqhKhkr1OOk=";
+      url = "https://dl.discordapp.net/apps/linux/0.0.17/discord-0.0.17.tar.gz";
+      sha256 = "sha256-NGJzLl1dm7dfkB98pQR3gv4vlldrII6lOMWTuioDExU=";
     };
   });
 in {
