8447cc0099108b748e8cbeab46fc199710553ec0
localhost <localhost@irunx.org>
2024-01-30 12:41:10 +0100

    Add all-the-icons font

--- nixos-modules/fonts.nix
+++ nixos-modules/fonts.nix
@@ -5,11 +5,17 @@ let
 in {
   options.teletypeOne.fonts = {
     firaCode = mkEnableOption "Install the firacode font.";
+    all_the_icons = mkEnableOption "Install the all-the-icons (emacs) font.";
   };
 
   config = (mkMerge [
     (mkIf cfg.firaCode {
-      fonts.packages = with pkgs; [fira-code];
+      fonts.packages = with pkgs; [ fira-code ];
     })
+
+    (mkIf cfg.firaCode {
+      fonts.packages = with pkgs; [ emacs-all-the-icons-fonts ];
+    })
+
   ]);
 }
