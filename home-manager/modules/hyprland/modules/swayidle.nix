aaf3b2f8ffff043831e47f4840480ce5c3d31743
localhost <localhost@irunx.org>
2024-02-17 19:12:07 +0100

    Update swayidle config

--- home-manager/modules/hyprland/modules/swayidle.nix
+++ home-manager/modules/hyprland/modules/swayidle.nix
@@ -9,5 +9,12 @@ in
    };
    config = mkIf cfg.enable {
      home.packages = with pkgs; [ swayidle ]; 
+     home.file.".config/swayidle/config".text = ''
+        timeout 360 'light -O && light -S 5%' resume 'light -I' 
+        timeout 720 'swaylock'
+        timeout 1000 'light -O && light -S 1%' resume 'light -I'
+        timeout 1800 'systemctl suspend'
+        before-sleep 'swaylock'
+     '';
    };
 }
