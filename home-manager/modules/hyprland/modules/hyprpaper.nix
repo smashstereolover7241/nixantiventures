790da22a7b82fd169b1d5920bc91d33d3ae5e5c3
localhost <localhost@irunx.org>
2024-02-17 20:45:15 +0100

    Add hyprpaper config file

--- home-manager/modules/hyprland/modules/hyprpaper.nix
+++ home-manager/modules/hyprland/modules/hyprpaper.nix
@@ -10,5 +10,22 @@ in
    };
    config = mkIf cfg.enable {
      home.packages = with pkgs; [ hyprpaper ]; 
+     home.file.".config/hypr/hyprpaper.conf".text = ''
+        preload = ~/papers/a.png
+        #if more than one preload is desired then continue to preload other backgrounds
+        # .. more preloads
+
+        #set the default wallpaper(s) seen on initial workspace(s) --depending on the number of monitors used
+        wallpaper = ,~/papers/a.png
+        #if more than one monitor in use, can load a 2nd image
+        #wallpaper = monitor2,/path/to/next_image.png
+        # .. more monitors
+
+        #disable splash text rendering over the wallpaper
+        splash = false
+
+        #fully disable ipc
+        # ipc = off
+    '';
    };
 }
