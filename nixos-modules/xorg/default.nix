25d16abee2f56b438552f56a6ce69b81b940759f
localhost <localhost@irunx.org>
2024-02-18 11:11:44 +0100

    Debloat gtkgreet

--- nixos-modules/xorg/default.nix
+++ nixos-modules/xorg/default.nix
@@ -12,12 +12,7 @@ let
   '';
     swayConfig = pkgs.writeText "greetd-sway-config" ''
     # `-l` activates layer-shell mode. Notice that `swaymsg exit` will run after gtkgreet.
-    exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
-    bindsym Mod4+shift+e exec swaynag \
-      -t warning \
-      -m 'What do you want to do?' \
-      -b 'Poweroff' 'systemctl poweroff' \
-      -b 'Reboot' 'systemctl reboot'
+    exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet;"
   '';
   cfg = config.teletypeOne.xorg;
 in {
