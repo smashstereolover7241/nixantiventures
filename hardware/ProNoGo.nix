a45eb60d2dba46a2449b3c70edb034bca7cc5a10
localhost <localhost@irunx.org>
2024-04-21 15:18:35 +0200

    ProNoGo no longer gets amd modules on boot

--- hardware/ProNoGo.nix
+++ hardware/ProNoGo.nix
@@ -11,7 +11,7 @@ in
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
 
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [  ];
   boot.extraModulePackages = [ ];
 
   services.qemuGuest.enable = true;
