5d0c476ca86ccc660d00918ec5c372d3f0d06f15
localhost <localhost@irunx.org>
2024-06-11 16:50:23 +0200

    Fix nvidia opengl

--- nixos-modules/xorg/default.nix
+++ nixos-modules/xorg/default.nix
@@ -227,7 +227,12 @@ in {
 
     (mkIf (cfg.gpu == "nvidia") {
       services.xserver.videoDrivers = ["nvidia"];
-      hardware.opengl.enable = true;
+
+      hardware.opengl = {
+        enable = true;
+        driSupport = true;
+        driSupport32Bit = true;
+      };
 
       environment.systemPackages = mkIf (cfg.nvidia.prime)
       [ nvidia-offload pkgs.libglvnd ];
