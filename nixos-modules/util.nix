a5010935d1d68a1ff40fd40f3f5984f6774b72a9
localhost <localhost@irunx.org>
2024-06-21 20:31:53 +0200

    Add wdisplays to hyprlandUtils

--- nixos-modules/util.nix
+++ nixos-modules/util.nix
@@ -49,7 +49,7 @@ in {
 
     (mkIf cfg.hyprlandUtil {
       environment.systemPackages = with pkgs; [light hyprshot hyprland-protocols xdg-desktop-portal-hyprland
- hyprshade wmctrl wofi gmrun dunst arandr acpilight playerctl alsa-utils nwg-drawer];
+ hyprshade wmctrl wofi gmrun dunst arandr acpilight playerctl alsa-utils nwg-drawer wdisplays];
     })
 
     (mkIf cfg.swaylockFix {
