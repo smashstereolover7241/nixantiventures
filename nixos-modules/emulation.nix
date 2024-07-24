abb86a0db897657f954a840188046ff3176c4248
localhost <localhost@irunx.org>
2024-07-24 13:21:04 +0200

    Update qemu

--- nixos-modules/emulation.nix
+++ nixos-modules/emulation.nix
@@ -5,14 +5,30 @@ let
 in {
   options.teletypeOne.emulation = {
     enabled = mkEnableOption "Install qemu";
+    kvm = mkEnableOption "Install virtmanager";
     virtmanager = mkEnableOption "Install virt-manager";
   };
 
   config = (mkMerge [
     (mkIf (cfg.enabled) {
       environment.systemPackages = with pkgs; [qemu];
-      virtualisation.libvirtd.enable = true;
+      virtualisation.libvirtd = {
+      enable = true;
+      qemu = {
+         package = pkgs.qemu_kvm;
+	 swtpm.enable = true;
+	 ovmf = {
+	    enable = true;
+	    packages = [(pkgs.OVMF.override {
+	       secureBoot = true;
+	       tpmSupport = true;
+	    }).fd];
+	};
+      };
+      };
     })
+
+
     (mkIf (cfg.virtmanager) {
       programs.virt-manager.enable = true;
       users.users.localhost.extraGroups = ["libvirtd"];
