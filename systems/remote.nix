ca0680b0878309a4fe717288ef96681e86715639
localhost <localhost@irunx.org>
2024-08-11 19:03:26 +0200

    Fix typo

--- systems/remote.nix
+++ systems/remote.nix
@@ -194,7 +194,7 @@ inputs: {
         };
       };
       virtualisation.docker.enable = true;
-      virtualisation.docker.storagedriver = "zfs";
+      virtualisation.docker.storageDriver = "zfs";
     })
   ];
 }
