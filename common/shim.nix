df0513b5e7cd6df4ea93a1e0925054db6d809b81
localhost <localhost@irunx.org>
2024-10-22 14:53:18 +0200

    Formatting, temporarily remove hm config

--- common/shim.nix
+++ common/shim.nix
@@ -4,34 +4,32 @@ with lib;
 
 let cfg = config.shim;
 in {
-  imports = [./test.nix ./modules];
-  options.shim.enable = mkEnableOption "Enables the shim or something, why would this be deactivated?";
-  options.shim.homeTest = mkEnableOption "test me!";
-  options.shim.boolTest = mkOption {
-    description = "Yes or no.";
+imports = [./test.nix ./modules];
+    options.shim.enable = mkEnableOption "Enables the shim or something, why would this be deactivated?";
+    options.shim.homeTest = mkEnableOption "test me!";
+    options.shim.boolTest = mkOption {
+        description = "Yes or no.";
         type = types.bool;
-            default = true;
-  };
+        default = true;
+    };
     options.shim = {
         flakes = mkOption {
             description = "Enable flakes. As this is a flake, it is enabled by default.";
             type = types.bool;
             default = true;
         };
-        userss = mkOption {
+        users = mkOption {
             description = "Enable users. For now just yes or no.";
             type = types.bool;
             default = true;
         };
     };
-  config = mkIf cfg.enable (mkMerge [
-    (mkIf cfg.boolTest {
-        shim.test.enable = false;
-    })
-    (mkIf cfg.userss {
-        real.normal.system.users.enable = true;
-#         shim.userss.enable = true;
-#        normals.system.userss = true;
-    })
-  ]);
+    config = mkIf cfg.enable (mkMerge [
+        (mkIf cfg.boolTest {
+            shim.test.enable = false;
+        })
+        (mkIf cfg.users {
+            real.normal.system.users.enable = true;
+        })
+    ]);
 }
