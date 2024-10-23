ec1b245c816c5a1ad66817b32f510cb0248a2060
localhost <localhost@irunx.org>
2024-10-23 14:20:40 +0200

    Export more things

--- systems/stationary/babysfirstsystem/default.nix
+++ systems/stationary/babysfirstsystem/default.nix
@@ -15,27 +15,12 @@ in
                       ./settings.nix
           ];
           }
-          ({ pkgs, config, lib, home-manager, ... }: {
-            networking = {
-            hostName = systemName;
-            useDHCP = false;
-            hostId = "5457b35a";
-            };
-            #TODO: make this it's own module with pass-through args, so defaults can be changed easily
+          ({ pkgs, config, lib, ... }: {
 
-            nixpkgs.config.allowUnfree = true;
+            imports = [
+              (import ./common.nix {inherit pkgs config lib; systemName = systemName;}) # (1)
+            ];
 
-            time.timeZone = "Europe/Berlin";
-            system.stateVersion = "21.05";
-            hardware.enableRedistributableFirmware = true;
-            # Network configuration.
-            networking.firewall.allowedTCPPorts = [ 80 ];
-
-            # Enable a web server.
-            services.httpd = {
-              enable = true;
-              adminAddr = "morty@example.org";
-            };
           })
 
         ];
