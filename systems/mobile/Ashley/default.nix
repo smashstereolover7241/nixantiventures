99cbef4553368159052982cfeebb4f5cf8768a48
localhost <localhost@irunx.org>
2024-10-24 09:13:04 +0200

    Formatting

--- systems/mobile/Ashley/default.nix
+++ systems/mobile/Ashley/default.nix
@@ -3,23 +3,23 @@ let
   systemName = builtins.toString  (builtins.match ".*[/](.*)$" (builtins.toString ./.));
 in
 {
-    nixosConfigurations.${systemName}  = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules =
-        [
-          home-manager.nixosModules.home-manager
-          {
-          imports = [
-            ./hardware.nix
-            ./settings.nix
-          ];
-          }
-          ({ pkgs, config, lib, ... }@inputs: {
-              home-manager.extraSpecialArgs = { inherit inputs;};
-              imports = [
-                (import (toString self + "/systems/common.nix") {inherit pkgs config lib; systemName = systemName;})
-              ];
-          })
+  nixosConfigurations.${systemName}  = nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    modules =
+    [
+      home-manager.nixosModules.home-manager
+      {
+        imports = [
+          ./hardware.nix
+          ./settings.nix
         ];
-    };
+      }
+      ({ pkgs, config, lib, ... }@inputs: {
+        home-manager.extraSpecialArgs = { inherit inputs;};
+        imports = [
+          (import (toString self + "/systems/common.nix") {inherit pkgs config lib; systemName = systemName;})
+        ];
+      })
+    ];
+  };
 }
