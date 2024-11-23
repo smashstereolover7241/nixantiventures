32085e35eb8f523736c3f6cf3b4e8864352f9345
localthehost <localhost@irunx.org>
2024-11-23 20:43:12 +0100

    Port xmobar & waybar

--- common/modules/normal/display/default.nix
+++ common/modules/normal/display/default.nix
@@ -4,5 +4,6 @@
   imports = [
     ./desktop-environments
     ./window-managers
+    ./generic
   ];
 }
---
+++ common/modules/normal/display/generic/bars.nix
@@ -0,0 +1,16 @@
+{ config, lib, pkgs, ... }:
+with lib;
+let
+  cfg = config.real.normal.display.generic.bars;
+in
+{
+  options.real.normal.display.generic.bars = {
+    waybar = mkEnableOption "Install waybar";
+    xmobar = mkEnableOption "Install xmobar";
+  };
+
+  config = (mkMerge [
+     { environment.systemPackages = mkIf cfg.xmobar (with pkgs; [xmobar]); }
+     { environment.systemPackages = mkIf cfg.waybar (with pkgs; [waybar]); }
+  ]);
+}
