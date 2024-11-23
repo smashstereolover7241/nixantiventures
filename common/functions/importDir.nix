192cbcc0f0caf700c2d56b81e3870cb741047d83
localthehost <localthehost@irunx.org>
2024-11-23 18:45:03 +0100

    Remove leftover directory

--- common/functions/importDir.nix
+++ common/functions/importDir.nix
@@ -5,5 +5,6 @@ let
   nixFiles = lib.filterAttrs (n: v: (((builtins.match "^.+nix" n) == []) && ((builtins.match "^default\.nix$" n) != [])) || v == "directory" ) dirContents;
 in
 {
+  #TODO: Check if folders have default.nix; otherwise ignore and warn!
    importList = lib.forEach (lib.attrNames nixFiles) (x: dirToScan + ( "/" +x ));
 }
