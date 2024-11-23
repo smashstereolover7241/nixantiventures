a70c51a2afafff243b20124d025112001e8aaa2f
localthehost <localthehost@irunx.org>
2024-11-23 18:53:39 +0100

    Add xmonad module

--- common/modules/normal/display/default.nix
+++ common/modules/normal/display/default.nix
@@ -3,5 +3,6 @@
 {
   imports = [
     ./desktop-environments
+    ./window-managers
   ];
 }
--- common/modules/normal/display/window-managers/xmonad.nix
+++ common/modules/normal/display/window-managers/xmonad.nix
@@ -1,11 +1,21 @@
 { config, lib, pkgs, ... }:
 with lib;
 let
-  cfg = config.real.normal.display.window-managers.xmonad
+  cfg = config.real.normal.display.window-managers.xmonad;
 in
 {
   options.real.normal.display.window-managers.xmonad = {
     enable = mkEnableOption "Enable xmoand";
-  }
-
+  };
+  config = (mkMerge [
+    (mkIf cfg.enable {
+      services = {
+        xserver.windowManager = {
+          xmonad.enable = true;
+          xmonad.enableContribAndExtras = true;
+        };
+        displayManager.defaultSession = "best-wm-xmonad";
+      };
+    })
+  ]);
 }
